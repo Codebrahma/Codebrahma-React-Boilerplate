41ad3bcbedc031326cc1f34f3c87e1fd43fdf33b
prasanna1211 <prasanna@codebrahma.com>
2018-04-21 12:42:01 +0530

    added airbnb eslint + lint rules

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
         }
       },
       {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
         test: /\.css$/,
         use: [
           {
