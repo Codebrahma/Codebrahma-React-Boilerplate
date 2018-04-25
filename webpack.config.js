75a382fb3e484de3da9e2dbda8da4ffcf0df080b
ankushCb <ankush@codebrahma.com>
2018-04-25 17:28:30 +0530

    Webpack production configuration and sideEffects in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,7 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+module.exports = merge(common, {
+   devtool: 'inline-source-map',
+   mode: 'development',
 });
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-        }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
-            loader: "sass-loader" // compiles Sass to CSS
-        }]
-      },
-    ]
-  },
-  plugins: [htmlWebpackPlugin]
-};
