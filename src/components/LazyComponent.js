db5de8414a4bcd280f22e18d00662ffbf3d1fd3b
errorr404 <dkbishwash@gmail.com>
2019-03-08 13:16:49 +0530

    added lazy loading

---
+++ src/components/LazyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const LazyComponent = () => (
+  <div>
+    <p> Lazy Component </p>
+  </div>
+);
+
+export default LazyComponent;
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
-import Dashboard from './components/Dashboard';
-import Home from './components/Home';
+const render = Comp => props => (<Comp {...props} />);
 
+const Dashboard = render(lazy(() => import('./components/Dashboard')));
+const Home = render(lazy(() => import('./components/Home')));
 
 const Routes = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/dashboard" component={Dashboard} />
-    </Switch>
-  </Router>
+  <div>
+    <Suspense fallback={<div>loading...</div>}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </Suspense>
+  </div>
 );
 export default Routes;
