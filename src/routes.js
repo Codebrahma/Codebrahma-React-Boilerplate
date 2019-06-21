008241c7d8169ec4d9515f7e29693ff2126d62e7
DivyanshBatham <divyansh.sunita@gmail.com>
2019-06-21 16:19:56 +0530

    Updated react-router-dom from 4.3.1 to 5.0.1

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
-const render = Comp => props => (<Comp {...props} />);
-const Dashboard = render(lazy(() => import('./components/Dashboard')));
-const Home = render(lazy(() => import('./components/Home')));
+const Dashboard = lazy(() => import('./components/Dashboard'));
+const Home = lazy(() => import('./components/Home'));
 
 const Routes = () => (
   <div>
